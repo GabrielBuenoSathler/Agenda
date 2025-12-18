ab20752251c110e0903c3d84c6a9f0ba5eb6b717
gabriel <gabrielbuenosathler@gmail.com>
2025-12-17 22:23:46 -0500

    adicionando databases

---
+++ src/agenda/models.py
@@ -0,0 +1,72 @@
+
+from datetime import datetime
+from sqlalchemy.orm import (
+    Mapped, mapped_as_dataclass, registry, mapped_column, relationship
+)
+from sqlalchemy import func, ForeignKey
+
+table_registry = registry()
+
+
+@mapped_as_dataclass(table_registry)
+class User:
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str]
+    email: Mapped[str] = mapped_column(unique=True)
+    created_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now()
+    )
+    Evento: Mapped[list['Evento']] = relationship(
+        init=False,
+        cascade='all, delete-orphan',
+        lazy='selectin',
+    )
+
+    
+@mapped_as_dataclass(table_registry)
+class Evento:
+    __tablename__ = "Evento"
+
+    id_evento: Mapped[int] = mapped_column(init=False, primary_key=True)
+
+    # Foreign key: cada Book pertence a UM User
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+
+    nome_da_tarefa: Mapped[str]
+    data_tarefa: Mapped[datetime]
+    data_limite: Mapped[datetime]
+    lugar: Mapped[str]
+    problema: Mapped[datetime] 
+    solucao: Mapped[str]  
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/agenda/schema.py
@@ -0,0 +1,35 @@
+from pydantic  import BaseModel , EmailStr
+import datetime 
+
+class UserPublic(BaseModel):
+    id : int 
+    username : str
+    email : EmailStr
+    password : str 
+
+
+class UserSchema(BaseModel):
+    email : str 
+    username : str
+    password : str
+
+
+class EventoPublic(BaseModel):
+    usuario_id : int 
+    id_evento : int 
+    nome_evento : str
+    problema : str 
+    data_de_criacao : datetime
+    data_limite : datetime 
+    lugar : str
+    usuario_atendimento : str  
+    solucao : str 
+
+
+class EventoSchema(BaseModel): 
+    problema : str
+    data_de_criacao : datetime      
+    data_limite : datetime          
+    lugar : str                     
+    usuario_atendimento : str       
+    solucao : str                   
